1220c12e16f7bd7eeebabbc66d052118f68f5e91
strange <strange.wang@neutec.com.tw>
2023-06-24 10:57:25 +0800

    add header navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-recipe-cart';
+  currentTab = 'recipes';
+
+  onChangeTab(tab: string) {
+    this.currentTab = tab;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() onChangeTab: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(tab: string) {
+    this.onChangeTab.emit(tab);
+  }
 }
